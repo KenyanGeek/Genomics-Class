de062a3c17c64f212075400248879c4694f02cea
KenyanGeek <hchege@uvm.edu>
2020-02-05 10:02:55 -0500

    added mypipeline script
    This is the script to map our data

---
+++ mypipeline.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+# We'll use this as a wrapper to run our different mapping scripts
+# Path to my repo
+myrepo="/users/h/c/hchege/Genomics-Class"
+
+# My population:
+mypop="XCS"
+
+
+# Directory to our cleaned and paired reads
+input="/data/project_data/RS_ExomeSeq/fastq/edge_fastq/pairedcleanreads/${mypop}"
+
+
+# Directory to store the outputs of our mapping
+
+output="/data/project_data/RS_ExomeSeq/mapping"
+
+
+
+
+
