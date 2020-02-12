af315445285b93f87db63787560061a827704de2
KenyanGeek <hchege@uvm.edu>
2020-02-12 09:51:51 -0500

    added bamstats script

---
+++ myscripts/mapping copy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# set repo
+
+myrepo="/users/h/c/hchege/Genomics-Class"
+
+mypop="XCS"
+
+output="/data/project_data/RS_ExomeSeq/mapping"
+
+echo "Num.reads R1 R2 Paired MateMapped Singletons MateMappedDiffChr" >${myrepo}/myresults/${mypop}.flagstats.txt
